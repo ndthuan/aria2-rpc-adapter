70ed1d5f8aad90f9c43071e7fad7de00d70dcfd2
Thuan Nguyen <thuan@nguyens.xyz>
2016-03-12 12:30:58 +0700

    Initial version
    Move example into README
    
    Merge call and __call
    
    Update README with info about aria2

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # aria2-rpc-adapter
-Simple Aria2 RPC Adapter
+Simple Aria2 RPC Adapter. More info about Aria2: https://aria2.github.io/.
+
+# How to enable RPC mode
+* Simple mode: `aria2c --enable-rpc --rpc-listen-all`
+* Token-enabled mode: `aria2c --enable-rpc --rpc-listen-all --rpc-secret=MySecretToken`
+
+# Adapter usage example
+```php
+<?php
+
+require 'vendor/autoload.php';
+
+$client = new \JsonRPC\Client('http://localhost:6800/jsonrpc');
+$adapter = new \Ndthuan\Aria2RpcAdapter\Adapter($client);
+
+print_r(
+    $adapter->getGlobalStat()
+);
+
+var_dump(
+    $adapter->addUri(
+        ['http://google.com'],
+        ['dir' => getenv('HOME') . '/Downloads']
+    )
+);
+
+print_r(
+    $adapter->tellActive()
+);
+```
---
+++ src/Adapter.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Ndthuan\Aria2RpcAdapter;
+
+use JsonRPC\Client;
+
+/**
+ * Aria2 RPC Adapter
+ *
+ * For complete list of methods, see https://aria2.github.io/manual/en/html/aria2c.html#methods
+ *
+ * @method array getGlobalStat
+ * @method string addUri(array $uris, array $options = []) Returns the GID of the newly registered download.
+ * @method array tellActive(array $key = [])
+ */
+class Adapter
+{
+    /**
+     * @var Client
+     */
+    private $client;
+
+    /**
+     * @var string
+     */
+    private $token;
+
+    /**
+     * Adapter constructor.
+     *
+     * @param Client $client
+     * @param $token
+     */
+    public function __construct(Client $client, $token = '')
+    {
+        $this->client = $client;
+        $this->token = $token;
+    }
+
+    /**
+     * Make a call to Aria2 RPC
+     *
+     * @param string $name
+     * @param array $arguments
+     *
+     * @return mixed
+     */
+    public function __call($name, $arguments)
+    {
+        if ($this->token) {
+            array_unshift($arguments, 'token:' . $this->token);
+        }
+
+        return $this->client->execute('aria2.' . $name, $arguments);
+    }
+}
